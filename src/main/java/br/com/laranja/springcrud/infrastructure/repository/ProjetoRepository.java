83111ebf0cd53031103ab7661138aeebe589f6d4
Lucas Cezario <lucasdsc96@gmail.com>
2021-08-07 19:49:52 -0300

    Jpa no repository de projeto

--- src/main/java/br/com/laranja/springcrud/infrastructure/repository/ProjetoRepository.java
+++ src/main/java/br/com/laranja/springcrud/infrastructure/repository/ProjetoRepository.java
@@ -1,6 +1,7 @@
 package br.com.laranja.springcrud.infrastructure.repository;
 
 import br.com.laranja.springcrud.domain.model.Projeto;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,7 +9,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface ProjetoRepository extends CrudRepository<Projeto, Long> {
+public interface ProjetoRepository extends JpaRepository<Projeto, Long> {
     List<Projeto> findAll();
 
     @Override

325a4e544522fc79315e07c8d48f83421708b5f2
CarlosDanielS3 <carlos.daniel@mega.com.br>
2021-08-12 21:41:43 -0300

    required field version validation

--- src/main/java/br/com/laranja/springcrud/apllication/controller/ErrorHandler.java
+++ src/main/java/br/com/laranja/springcrud/apllication/controller/ErrorHandler.java
@@ -1,9 +1,12 @@
 package br.com.laranja.springcrud.apllication.controller;
 
 
+import br.com.laranja.springcrud.domain.dto.erro.ErroDeFormularioDto;
 import br.com.laranja.springcrud.domain.service.LogService;
 import br.com.laranja.springcrud.infrastructure.exception.*;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -11,12 +14,14 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestControllerAdvice
 @RequiredArgsConstructor
 public class ErrorHandler {
     private final LogService loger;
+    private final MessageSource messageSource;
 
     @ExceptionHandler(UsuarioNotFoundException.class)
     public ResponseEntity<String> usuarioNotFound(UsuarioNotFoundException e){
@@ -74,9 +79,16 @@ public class ErrorHandler {
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<List<FieldError>> FieldNotFound(MethodArgumentNotValidException e){
-        loger.log(e);
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getFieldErrors());
+    public ResponseEntity<List<ErroDeFormularioDto>> FieldNotFound(MethodArgumentNotValidException exception){
+        List<ErroDeFormularioDto> dto = new ArrayList<>();
+
+        List<FieldError> fieldErrors = exception.getBindingResult().getFieldErrors();
+        fieldErrors.forEach(e -> {
+            String mensagem = messageSource.getMessage(e, LocaleContextHolder.getLocale());
+            ErroDeFormularioDto erro = new ErroDeFormularioDto(e.getField(), mensagem);
+            dto.add(erro);
+        });
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(dto);
     }
 }
 

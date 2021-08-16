fe662306fa769485610eaca50e903816a243d3e7
CarlosDanielS3 <carlos.daniel@mega.com.br>
2021-08-15 23:15:53 -0300

    swagger

--- src/main/java/br/com/laranja/springcrud/SpringCrudApplication.java
+++ src/main/java/br/com/laranja/springcrud/SpringCrudApplication.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 @SpringBootApplication
 @EnableCaching
 @EnableFeignClients
+@EnableSwagger2
 public class SpringCrudApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCrudApplication.class, args);
